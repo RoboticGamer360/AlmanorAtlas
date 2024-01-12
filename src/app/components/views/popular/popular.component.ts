274e1fea5ce2de7fde038a74fd0450593703c8f0
Ashwin <mrc93074@gmail.com>
2024-01-11 20:43:20 -0800

    Popular page

--- src/app/components/views/popular/popular.component.ts
+++ src/app/components/views/popular/popular.component.ts
@@ -1,7 +1,25 @@
+import { CommonModule } from "@angular/common";
 import { Component } from "@angular/core";
 
 @Component({
   standalone: true,
   templateUrl: './popular.component.html',
-  styleUrls: ['./popular.component.scss']
-}) export class PopularComponent {}
+  styleUrls: ['./popular.component.scss'],
+  imports: [
+    CommonModule
+  ]
+}) export class PopularComponent {
+  hotSpots = [{
+    title: 'Title, Inc.',
+    description: 'A very super cool interesting place to be, tbh',
+    bgColor: '#dff5fe'
+  }, {
+    title: 'Nuclear Party',
+    description: 'Such a Blast!',
+    bgColor: '#E1308A0D'
+  }, {
+    title: 'Ball pit',
+    description: 'jump on in',
+    bgColor: '#c8ffbf'
+  }]
+}
