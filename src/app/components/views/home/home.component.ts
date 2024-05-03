eeb81147aa7606d9c6ad431eb915fa0d63780729
Nicholas Walkewicz <nwalkewicz@gmail.com>
2024-05-03 04:00:08 -0700

    Replace hrefs with routerLink

--- src/app/components/views/home/home.component.ts
+++ src/app/components/views/home/home.component.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from "@angular/common";
 import { Component, OnInit } from "@angular/core";
+import { RouterModule } from "@angular/router";
 
 @Component({
   standalone: true,
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   templateUrl: './home.component.html',
   styleUrls: [ './home.component.scss' ]
